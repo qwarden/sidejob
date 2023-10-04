070ab1dad347db28f4924952b4f182f65e5a5e4f
Quimby Warden <quimbywarden@gmail.com>
2023-10-04 14:40:57 -0400

    add app structure

---
+++ SideJob/SideJob/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SideJob
+//
+//  Created by Quimby Warden on 10/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SideJob/SideJob/SideJobApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideJobApp.swift
+//  SideJob
+//
+//  Created by Quimby Warden on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SideJobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
