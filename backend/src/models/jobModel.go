a2c703a900d82e3334b6ea3290407729328610fd
Quimby Warden <quimbywarden@gmail.com>
2023-10-18 15:02:13 -0400

    backend organized

--- backend/src/models/jobModel.go
+++ backend/src/models/jobModel.go
@@ -4,20 +4,11 @@ import (
   "gorm.io/gorm"
 )
 
-type User struct {
-  gorm.Model
-  ID       uint
-  Name     string
-  Email    string `gorm:"type:varchar(100);uniqueIndex"`
-  Password string
-  Jobs     []Job
-}
-
 type Job struct {
+  gorm.Model
   ID          uint
   Name        string
   Address     string
   Description string
   UserID      uint
 }
-
