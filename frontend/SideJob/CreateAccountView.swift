e6b565b2b1cd0d8fb07465afccdaa4757467dcfe
larsjensen <lgjensen5@gmail.com>
2023-10-31 12:35:55 -0400

    Good start with some front end validation

--- frontend/SideJob/CreateAccountView.swift
+++ frontend/SideJob/CreateAccountView.swift
@@ -8,47 +8,99 @@
 import SwiftUI
 
 struct CreateAccountView: View {
+    //User variables
     @State private var email = ""
     @State private var name = ""
     @State private var password = ""
     @State private var passwordConfirm = ""
-    @State private var passwordErrorMessages = ""
+    
+    // Error messages
+    @State private var nameErrorMessages = ""
     @State private var emailErrorMessages = ""
-
+    @State private var passwordErrorMessageEmpty = ""
+    @State private var passwordErrorMessageMatch = ""
+    @State private var accountCreationErrorMessages = ""
     
     var body: some View {
-        Text("Create your Account:").font(.system(size: 24, weight: .bold, design: .default)).padding()
+        Text("Create your Account:").font(.system(size: 24, weight: .bold, design: .default)).foregroundColor(Color(.systemBlue)).padding()
         VStack(spacing: 25) {
             HStack { Text("Name"); TextField("name", text: $name) }
+            //error messages go here
+            if (nameErrorMessages != "") {
+                Text(nameErrorMessages).foregroundColor(Color.red)
+            }
             HStack { Text("Email"); TextField("email", text: $email) }
+            if (emailErrorMessages != "") {
+                Text(emailErrorMessages).foregroundColor(Color.red)
+            }
             HStack { Text("Password"); SecureField("password", text: $password) }
+            if (passwordErrorMessageEmpty != "") {
+                Text(passwordErrorMessageEmpty).foregroundColor(Color.red)
+            }
             HStack { Text("Confirm Password"); SecureField("password", text: $passwordConfirm) }
+            if (passwordErrorMessageMatch != "") {
+                Text(passwordErrorMessageMatch).foregroundColor(Color.red)
+            }
             Button("Create Account", action: attemptCreateAccount).buttonStyle(.bordered)
             
             //error messages go here
-            Text(passwordErrorMessages)
-            Text(emailErrorMessages)
+            if (accountCreationErrorMessages != ""){
+                Text(accountCreationErrorMessages)
+            }
             
         }.padding()
     }
     
     func attemptCreateAccount(){
-        // first check if passwords match
-        if (password != passwordConfirm) {
-            passwordErrorMessages = "Passwords do not match."
+        if (frontEndChecks()) {
+            // where we check with backend to see if user creation is valid
+//            if (true) {
+//                // update the observable object for the user
+//                FeedView()
+//            }
+//            else {
+//                accountCreationErrorMessages = "Sorry, there were issues on our side creating your account."
+//            }
         }
-        else {
-            passwordErrorMessages = ""
-        }
-        if (!email.contains("@")) {
-            emailErrorMessages = "Invalid Email. Must contain '@' character."
-        }
-        else {
-            emailErrorMessages = ""
+    }
+    
+    func frontEndChecks() -> Bool {
+        var frontendChecksPassed = true
+        
+        //reset error messages
+        nameErrorMessages = ""
+        emailErrorMessages = ""
+        passwordErrorMessageEmpty = ""
+        passwordErrorMessageMatch = ""
+        accountCreationErrorMessages = ""
+        
+        // name check
+        if (name == "") {
+            nameErrorMessages = "Name cannot be empty."
+            frontendChecksPassed = false
         }
         
-        // where we check with backed to see if user is valid
-        // if it works, set logged in to true
+        // email checks
+        if (email == "") {
+            emailErrorMessages = "Email cannot be empty."
+            frontendChecksPassed = false
+        }
+        else if (!email.contains("@")) {
+            emailErrorMessages = "Invalid Email. Must contain '@' character."
+            frontendChecksPassed = false
+        }
+        
+        // password checks
+        if (password == "") {
+            passwordErrorMessageEmpty = "Password cannot be empty."
+            frontendChecksPassed = false
+        }
+        else if (password != passwordConfirm) {
+            passwordErrorMessageMatch = "Passwords do not match."
+            frontendChecksPassed = false
+        }
+        
+        return frontendChecksPassed
     }
 }
 
--- frontend/SideJob/WelcomeView.swift
+++ frontend/SideJob/WelcomeView.swift
@@ -8,24 +8,44 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    @State private var loggedIn = false
+    
+    init(loggedIn: Bool = false) {
+        self.loggedIn = checkLoggedIn()
+    }
+    
     var body: some View {
-        VStack {
+        //if we haven't already logged in, we show welcome screen.
+        if !(loggedIn){
             LoginAccountNavigation()
         }
+        else {
+            FeedView()
+        }
     }
+    
+    func checkLoggedIn() -> Bool {
+        //here we implement checking the persistence to see if user has already logged in, if so we get that login info and update the user environment object
+        return false
+    }
+    
+    
 }
 
 struct LoginAccountNavigation: View {
     var body: some View {
-        Text("SideJob").font(.system(size: 48, weight: .bold, design: .default))
-            .foregroundColor(Color(.systemBlue)).padding(100)
         NavigationStack {
+            Spacer()
+            Text("SideJob").font(.system(size: 48, weight: .bold, design: .default))
+                .foregroundColor(Color(.systemBlue)).padding(100)
+            Spacer()
             NavigationLink(destination: LoginView()) {
                 Text("Login").font(.largeTitle)
             }.padding()
             NavigationLink(destination: CreateAccountView()) {
                 Text("Create Account").font(.largeTitle)
             }
+            Spacer()
         }
     }
 }
