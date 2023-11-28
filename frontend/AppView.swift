97ebddb933fe8430defd55318c4c4aa5688f130d
larsjensen <lgjensen5@gmail.com>
2023-11-28 11:11:47 -0500

    Profile integrated with welcome, sign out works properly

--- frontend/AppView.swift
+++ frontend/AppView.swift
@@ -2,7 +2,33 @@
 //  AppView.swift
 //  SideJob
 //
-//  Created by Lars Jensen on 11/28/23.
+//  Created by India Davis on 10/31/23.
 //
+import SwiftUI
 
-import Foundation
+struct AppView: View {
+    var body: some View {
+        NavigationView{
+            TabView() {
+                FeedView()
+                    .tabItem() {
+                        Image(systemName: "square.stack")
+                    }
+                ProfileView()
+                    .tabItem() {
+                        Image(systemName: "person")
+                    }
+
+            }
+
+        }
+    }
+
+}
+
+
+struct AppView_Preview: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
