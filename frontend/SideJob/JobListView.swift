f66a04f04d9e2ad70c5cd6354831d5d310e0c199
caelchristian <caelchristian23@gmail.com>
2023-12-05 00:02:52 -0500

    jobListView now fetches jobs from http client. Jobservice deleted

--- frontend/SideJob/JobListView.swift
+++ frontend/SideJob/JobListView.swift
@@ -7,59 +7,61 @@
 
 import SwiftUI
 
-
 struct JobListView: View {
-    // Using ObservedObject to observe changes in the shared JobService instance
-    @ObservedObject var jobService = JobService.shared
-    @State private var showingPostView = false
+    @EnvironmentObject var client: Client
+    @State var jobs: [Job] = []
+    @State var loadError: Bool = false
+    var endpoint: String
 
     var body: some View {
-        NavigationView {
-            ZStack(alignment: .bottomTrailing) {
-                // List of jobs, each represented by JobView
-                List(jobService.jobs) { job in
-                    JobView(job: job)
+            List {
+                if jobs.isEmpty && !loadError {
+                    Text("Loading jobs...")
+                } else if loadError {
+                    Button("Tap to retry") {
+                        fetchJobs()
+                    }
+                } else {
+                    ForEach(jobs) { job in
+                        JobView(job: job)
+                    }
                 }
-                .navigationBarTitle("Jobs")
-                // Fetch jobs when the view appears
-                .onAppear {
-                    jobService.fetchJobs()
-                }
-
-                // Floating action button to show the PostView
-                FloatingActionButton(action: {
-                    self.showingPostView = true
-                })
             }
-            // Present the PostView as a sheet
-            .sheet(isPresented: $showingPostView) {
-                PostView()
+            .onAppear {
+                fetchJobs()
+            }
+        }
+
+        private func fetchJobs() {
+            loadError = false
+            client.fetch(verb: "GET", endpoint: endpoint, auth: true) { result in
+                switch result {
+                case .success(let data):
+                    let decoder = JSONDecoder()
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+                    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+                    dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+                    decoder.dateDecodingStrategy = .formatted(dateFormatter)
+
+                    if let decodedJobs = try? decoder.decode([Job].self, from: data) {
+                        self.jobs = decodedJobs
+                    } else {
+                        self.loadError = true
+                        // TODO: handle decoding error
+                    }
+                case .failure(_):
+                    self.loadError = true
+                    // TODO: handle networking error
+                }
             }
         }
     }
-}
-
-// NewJob button
-struct FloatingActionButton: View {
-    var action: () -> Void
-
-    var body: some View {
-        Button(action: action) {
-            Image(systemName: "plus")
-                .resizable()
-                .frame(width: 24, height: 24)
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .clipShape(Circle())
-                .padding()
-        }
-    }
-}
 
 
-struct JobListView_Preview: PreviewProvider {
+
+struct JobListView_Previews: PreviewProvider {
     static var previews: some View {
-        FeedView()
+        JobListView(endpoint: "/jobs").environmentObject(Client())
     }
 }
