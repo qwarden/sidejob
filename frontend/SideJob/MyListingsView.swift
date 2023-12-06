15ebcde62212f7998e8572b74404daaaaeccdcfb
Quimby Warden <quimbywarden@gmail.com>
2023-12-05 20:49:07 -0500

    fix app build

--- frontend/SideJob/MyListingsView.swift
+++ frontend/SideJob/MyListingsView.swift
@@ -5,42 +5,33 @@
 //  Created by Lars Jensen on 10/17/23.
 //
 
-//import SwiftUI
-//
-//
-//
-//struct ListingsView: View {
-//       let user: UserInfo
-//
-//       init(user: UserInfo) {
-//           self.user = user
-//       }
-//    
-//    
-//    // Using ObservedObject to observe changes in the shared JobService instance
-//    @ObservedObject var jobService = JobService.shared
-//    @State private var showingPostView = false
-//    
-//    var body: some View {
-//      NavigationView {
-//            Text(user.name)
-//            ZStack(alignment: .bottomTrailing) {
-//                // List of jobs, each represented by JobView
-//                
-//                List(jobService.jobs.filter {$0.postedByID == user.userID}) { job in
-//                    JobView(job: job)
-//                }
-//                 
-//                // Fetch jobs when the view appears
-//                .onAppear {
-//                    jobService.fetchJobs()
-//                }
-//            }
-//      
-//            // Present the PostView as a sheet
-//            .sheet(isPresented: $showingPostView) {
-//                PostView()
-//            }
-//        }
-//    }
-//}
+import SwiftUI
+
+
+
+struct ListingsView: View {
+       let user: UserInfo
+
+       init(user: UserInfo) {
+           self.user = user
+       }
+    
+    
+    // Using ObservedObject to observe changes in the shared JobService instance
+    @State private var showingPostView = false
+    
+    var body: some View {
+      NavigationView {
+            Text(user.name)
+            ZStack(alignment: .bottomTrailing) {
+                // List of jobs, each represented by JobView
+                
+            }
+      
+            // Present the PostView as a sheet
+            .sheet(isPresented: $showingPostView) {
+                PostView()
+            }
+        }
+    }
+}
