7c454d464f56fd442b2466db4208a144b198c5a3
caelchristian <caelchristian23@gmail.com>
2023-12-06 21:26:29 -0500

    Rearranged buttons

--- frontend/SideJob/FeedView.swift
+++ frontend/SideJob/FeedView.swift
@@ -1,10 +1,3 @@
-//
-//  FeedView.swift
-//  SideJob
-//
-//  Created by Quimby Warden on 10/4/23.
-//
-
 import SwiftUI
 
 struct FeedView: View {
@@ -12,68 +5,51 @@ struct FeedView: View {
     @State private var showingFilterView = false
     @State var filteringByLocation = false
     @EnvironmentObject private var client: Client
-    @EnvironmentObject private var locationObject: LocationManager
-    
+
     var body: some View {
         NavigationView {
-            VStack {
-                JobListView(endpoint: "/jobs/", filteringByLocation: $filteringByLocation)
-                
-                HStack {
-                    FloatingActionButtonFilter(action: {
-                        self.showingFilterView = true
-                    })
-                    FloatingActionButtonPost(action: {
-                        self.showingPostView = true
-                    })
-                }
-                
-            }
-            .navigationBarTitle("Jobs")
-            .sheet(isPresented: $showingPostView) {
-                PostView()
-            }
-            .sheet(isPresented: $showingFilterView) {
-                FilterView(filteringByLocation: $filteringByLocation)
-            }
-        }
-    }
-    
-    struct FloatingActionButtonPost: View {
-        var action: () -> Void
-        
-        var body: some View {
-            Button(action: action) {
-                Image(systemName: "plus")
-                    .resizable()
-                    .frame(width: 24, height: 24)
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .clipShape(Circle())
-                    .padding()
-            }
-        }
-    }
-    
-    struct FloatingActionButtonFilter: View {
-        var action: () -> Void
-        
-        var body: some View {
-            Button(action: action) {
-                Text("Filter")
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .clipShape(Capsule())
-                    .padding()
-            }
-        }
-    }
-}
+            ZStack(alignment: .bottomTrailing) {
+                VStack {
+                    HStack {
+                        Text("Sidejobs")
+                            .font(.title)
+                            .padding(.leading)
+                        Spacer()
+                        Button(action: {
+                            self.showingFilterView = true
+                        }) {
+                            Image(systemName: "line.horizontal.3.decrease.circle")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                        }
+                        .padding(.trailing)
+                    }
 
-struct FeedView_Previews: PreviewProvider {
-    static var previews: some View {
-        FeedView().environmentObject(Client())
+                    JobListView(endpoint: "/jobs/", filteringByLocation: $filteringByLocation)
+
+                    Spacer()
+                }
+                .sheet(isPresented: $showingPostView) {
+                    PostView()
+                }
+                .sheet(isPresented: $showingFilterView) {
+                    FilterView(filteringByLocation: $filteringByLocation)
+                }
+
+                Button(action: {
+                    self.showingPostView = true
+                }) {
+                    Image(systemName: "plus")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 30, height: 30)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                        .padding(10)
+                }
+            }
+        }
     }
 }
